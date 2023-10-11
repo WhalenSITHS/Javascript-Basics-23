0bf36dd9f84a9c33675c5ccc81e143bd40855019
mr. whalen <michael.whalen@sitechhs.com>
2023-10-11 13:25:43 -0400

    s

---
+++ dom.js
@@ -0,0 +1,13 @@
+DOMSelectors = {
+  // store all related code for selecting elements here
+  form: document.querySelector("#form"),
+  firstName: document.querySelector(".first-name"),
+  h2s: document.querySelectorAll("h2"),
+};
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  DOMSelectors.h2s.forEach(
+    (header) => (header.textContent = DOMSelectors.firstName.value)
+  );
+});
